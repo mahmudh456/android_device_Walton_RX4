9ce5fe9f8563372b64013a9bea5b741ee1b3a9cd
mahmudh456 <mahmudhs949@gmail.com>
2016-04-05 22:12:32 +0600

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := K1_turbo
+PRODUCT_RELEASE_NAME := RX4
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/kingzone/k1_turbo/device_k1-turbo.mk)
+$(call inherit-product, device/Walton/RX4/device_RX4.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k1_turbo
-PRODUCT_NAME := cm_k1_turbo
-PRODUCT_BRAND := KINGZONE
-PRODUCT_MODEL := K1_turbo
-PRODUCT_MANUFACTURER := KINGZONE
+PRODUCT_DEVICE := RX4
+PRODUCT_NAME := cm_RX4
+PRODUCT_BRAND := Walton
+PRODUCT_MODEL := RX4
+PRODUCT_MANUFACTURER := Walton
